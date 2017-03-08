22b8386c355ca0af42bce7628715ae2986a8299a
aditya990 <aditya.panidepu@gmail.com>
2017-03-08 20:45:00 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,38 +1,25 @@
-## The following functions does the following
-  ##Function 1. Create a list that contains a functions of
-  ## a. create a matrix
-  ## b. return the matrix
-  ## c. set an inverse of matrix
-  ## d. return inverse of matrix
-  
-  ##Function 2. returns inverse of a matrix but first checks if the matrix inverse is already cached in the first function
-
-makeCacheMatrix <- function(x)
-  {
-   xinv <- NULL
-   set <- function(y)
-   {
-     x <<- y
-     xinv <<- NULL
-   }
-   get <- function() x
-   setinv <- function(inv) xinv <<- inv
-   getinv <- function() xinv
-   list(set = set,get = get, setinv = setinv,getinv = getinv)
+#The first function, makeMatrix creates a special "Matrix",
+#which is really a list containing a function to
+makeCacheMatrix <- function(x){
+  #set the value of the matrix
+  setMatrix <- function(y){
+    x <<- y
+    xinvrse <- NULL
   }
-
-
-## The following function gives the inverse of the matrix object entered
-cacheSolve <- function(x, ...) { 
-  inv <- x$getinv()
-  if(!is.null(inv))
-    {
-     message("getting cache data")
-     return (inv)
-    }
-  data <- x$get()
-  inv <- solve(data)
-  x$setinv(inv)
-  inv
-        ## Return a matrix that is the inverse of 'x'
+  #get the value of the matrix
+  getMatrix <- function() x
+  setInverseMatrix <- function(x) xinvrse <- solve(x) #set the inverse of the matrix
+  getInverseMatrix <- function(x) xinvrse #get the inverse of the matrix
+  list(set = setMatrix, get = getMatrix, setInverse = setInverseMatrix, getInverse = getInverseMatrix)
 }
+
+cacheSolve <- function(x,...){
+  xinvrse <- x$getinverse()
+  if(!isnull(xinvrse)){
+    message("getting cached data")
+    return(xinvrse)
+  }
+  data <- x$getMatrix()
+  xinvrse <- solve(data)
+  xinvrse
+  }
